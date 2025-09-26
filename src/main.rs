a8728f19bfb55588e69dc98fb1caaa487de7cb8b
Christopher Wilke <christopher.wilke86@googlemail.com>
2025-09-26 10:19:19 +0200

    Added initial Rust project

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
+use fancy_regex::Regex;
+
 fn main() {
-    println!("Hello, world!");
-    let hey = "test";
+    let raw_text = "Hello, this is a test!".to_string();
+
+    let re = Regex::new(r#""#)
+        .unwrap();
+
+    let preprocessed1: Vec<&str> = re.split(&raw_text[..]).map(|x| x.unwrap())
+        .collect();
+
+    println!("{}", raw_text);
 }
